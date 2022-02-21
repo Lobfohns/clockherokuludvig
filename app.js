7116ddd78bf72a2b280aea46aba942e04db2f38c
Lobfohns <ludv1428@stud.kea.dk>
2022-02-21 23:10:58 +0100

    clockstuff

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/clock", (req, res) => {
+    res.sendFile(__dirname + "/public/clock.html")
+});
+
+
+app.listen(process.env.PORT || "5000"), () => {
+    console.log("the server is running", 5000);
+});
